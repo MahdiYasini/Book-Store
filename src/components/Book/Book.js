bc853495b12aaae0d598316418a63f2a9731f1f8
Mahdi Yasini <34657907+MahdiYasini@users.noreply.github.com>
2020-04-13 13:45:48 +0430

    Ni

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-
 import withMonthNameDateAndTime from '../../services/timeHandler/timeHandler';
 
 const useStyles = makeStyles({
